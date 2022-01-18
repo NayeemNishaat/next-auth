0e494ffe33c50689594af16f33e820f549181ab3
nayeemjc <RxNayeemIMI@gmail.com>
2022-01-19 01:36:12 +0600

    'init'

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,45 @@
+import NextAuth from "next-auth/next";
+import Credentials from "next-auth/providers/credentials";
+import { connectToDatabase } from "../../../lib/db";
+import { verifyPassword } from "../../../lib/auth";
+
+// Note: It return a handler function!
+export default NextAuth({
+	session: { jwt: true },
+	providers: [
+		Credentials({
+			async authorize(credentials) {
+				const client = await connectToDatabase();
+
+				const usersCollection = client
+					.db()
+					.collection("users");
+
+				const user = await usersCollection.findOne({
+					email: credentials.email
+				});
+
+				if (!user) {
+					client.close();
+					throw new Error("No user found!");
+				}
+
+				// Important: await because async function returns a promise.
+				const isValid = await verifyPassword(
+					credentials.password,
+					user.password
+				);
+
+				if (!isValid) {
+					client.close();
+					throw new Error("Couldn't log in!");
+				}
+
+				client.close();
+				return {
+					email: user.email
+				}; // Note: returning {} will let next-auth know authorization succeeded.
+			}
+		})
+	]
+});
---
+++ pages/api/auth/signup.js
@@ -0,0 +1,53 @@
+import { connectToDatabase } from "../../../lib/db";
+import { hashPassword } from "../../../lib/auth";
+
+async function handler(req, res) {
+	if (req.method !== "POST") return;
+
+	const data = req.body;
+
+	const { email, password } = data;
+
+	if (
+		!email ||
+		!email.includes("@") ||
+		!password ||
+		password.trim().length < 7
+	) {
+		res.status(422).json({
+			message:
+				"Invalid Input. Password should also be at least 7 characters!"
+		});
+
+		return;
+	}
+
+	const client = await connectToDatabase();
+
+	const db = client.db();
+
+	// Important: Must await to fulfill the promise else we will get a promise object!
+	const existingUser = db.collection("users").findOne({
+		email: email
+	});
+
+	if (existingUser) {
+		res.status(422).json({
+			message: "User exists with this Email already!"
+		});
+		client.close();
+		return;
+	}
+
+	const hashedPassword = await hashPassword(password); // Important: Very careful with async tasks/promise. Because they return an object if we don't use await/then method to resolve the promise!
+
+	const result = await db.collection("users").insertOne({
+		email,
+		password: hashedPassword
+	});
+
+	res.status(201).json({ message: "Created User!" });
+	client.close();
+}
+
+export default handler;
